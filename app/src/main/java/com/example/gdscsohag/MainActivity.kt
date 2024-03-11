b9a5d15a02dfb8fb982aa7cbdad3d5e110bfdb53
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-12 00:37:41 +0200

    setup navigation components

--- app/src/main/java/com/example/gdscsohag/MainActivity.kt
+++ app/src/main/java/com/example/gdscsohag/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.gdscsohag
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.gdscsohag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +24,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        window.navigationBarColor = getColor(R.color.primary)
         setTheme(R.style.Base_Theme_GDSCSohag)
+        setupBottomNav()
+    }
+
+
+    private fun setupBottomNav() {
+        val navHost =
+            supportFragmentManager.findFragmentById(binding.mainFragment.id) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }

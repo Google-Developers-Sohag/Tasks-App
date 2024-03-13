5b564d4d5501ea5c4ab229a2d7ec1f9c0374b338
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-13 02:28:28 +0200

    create base fragment

---
+++ app/src/main/java/com/example/gdscsohag/ui/base/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.example.gdscsohag.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.example.gdscsohag.BR
+
+abstract class BaseFragment<DB : ViewDataBinding, VM : ViewModel> : Fragment() {
+    private var _binding: DB? = null
+    protected val binding: DB = _binding!!
+
+    abstract val layoutId: Int
+    abstract val viewModel: VM
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate<DB>(inflater, layoutId, container, false)
+            .apply {
+                lifecycleOwner = viewLifecycleOwner
+                setVariable(BR._all, viewModel)
+            }
+        return _binding!!.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}

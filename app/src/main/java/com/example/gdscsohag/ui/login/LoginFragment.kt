ba2b6e8b09030256a54532efbc0e1626a018fdb7
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-14 04:20:37 +0200

    setup appearance

--- app/src/main/java/com/example/gdscsohag/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/gdscsohag/ui/login/LoginFragment.kt
@@ -1,7 +1,9 @@
 package com.example.gdscsohag.ui.login
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
 import com.example.gdscsohag.MainActivity
 import com.example.gdscsohag.R
 import com.example.gdscsohag.databinding.FragmentLoginBinding
@@ -13,7 +15,21 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as MainActivity).controlBottomNavVisibility(false)
+        setupViewAppearance()
+    }
+
+
+    private fun setupViewAppearance() {
+        (activity as MainActivity).apply {
+            window.statusBarColor = getColor(R.color.primary)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                window.insetsController?.setSystemBarsAppearance(
+                   0,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+            }
+            controlBottomNavVisibility(false)
+        }
     }
 
 }

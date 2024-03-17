fa5d5db8ffac88b807e53d1a59a949f345622736
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-17 04:15:31 +0200

    update binding adapters

--- app/src/main/java/com/example/gdscsohag/ui/utils/BindingAdapter.kt
+++ app/src/main/java/com/example/gdscsohag/ui/utils/BindingAdapter.kt
@@ -5,13 +5,31 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.gdscsohag.ui.base.BaseAdapter
+import com.example.gdscsohag.ui.home.HomeUiState
+import com.example.gdscsohag.ui.home.adapter.HomeAdapter
+import com.example.gdscsohag.ui.profile.ProfileUiState
+import com.example.gdscsohag.ui.profile.adapter.ProfileAdapter
+import com.example.gdscsohag.ui.tasks.TasksUiState
+import com.example.gdscsohag.ui.tasks.adapter.TasksAdapter
 
 @BindingAdapter(value = ["app:setImageUrl"])
-fun ImageView.setImageUrl(url: String) {
-    Glide.with(this).load(url).into(this)
+fun ImageView.setImageUrl(url: String?) {
+    url?.let { Glide.with(this).load(it).into(this) }
+
 }
 
 @BindingAdapter(value = ["app:setRecyclerItems"])
-fun <T> RecyclerView.setRecyclerItems(items: List<T>) {
-    (this.adapter as BaseAdapter<T>).submitList(items)
+fun <T> RecyclerView.setRecyclerItems(items: List<T?>?) {
+    (this.adapter as BaseAdapter<T?>?)?.submitList(items)
 }
+
+@BindingAdapter(value = ["app:setHomeAdapter"])
+fun RecyclerView.setHomeAdapter(state: HomeUiState) {
+    adapter = HomeAdapter(state)
+}
+
+@BindingAdapter(value = ["app:setProfileAdapter"])
+fun RecyclerView.setProfileAdapter(state: ProfileUiState) {
+    adapter = ProfileAdapter(state)
+}
+

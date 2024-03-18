e129ff12c0309120a4afd5d54c16f43c4a838021
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-18 04:02:49 +0200

    implement content visibility

--- app/src/main/java/com/example/gdscsohag/ui/home/HomeUiState.kt
+++ app/src/main/java/com/example/gdscsohag/ui/home/HomeUiState.kt
@@ -1,7 +1,9 @@
 package com.example.gdscsohag.ui.home
 
 import com.example.gdscsohag.domain.Progress
+import com.example.gdscsohag.ui.base.ContentStatus
 
 data class HomeUiState(
+    val contentStatus: ContentStatus = ContentStatus.LOADING,
     val progressList: List<Progress> = listOf()
 )

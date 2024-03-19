e94ad6c01188949bd170c730ca1dfab6d38ce4c7
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-19 02:06:11 +0200

    add di dependency

--- app/src/main/java/com/example/gdscsohag/MainActivity.kt
+++ app/src/main/java/com/example/gdscsohag/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.gdscsohag.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript{
-    dependencies{
+buildscript {
+    dependencies {
         classpath(libs.androidx.navigation.safe.args.gradle.plugin)
     }
 }
@@ -8,4 +8,5 @@ buildscript{
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.androidHilt) apply false
 }

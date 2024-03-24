268ef80bd0114330cb8b0fdfc3691ec438b09146
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-03-24 02:40:09 +0200

    prepare tasks until review

--- app/src/main/java/com/example/gdscsohag/ui/tasks/TasksUiState.kt
+++ app/src/main/java/com/example/gdscsohag/ui/tasks/TasksUiState.kt
@@ -1,10 +1,9 @@
 package com.example.gdscsohag.ui.tasks
 
 import com.example.gdscsohag.domain.entity.Session
-import com.example.gdscsohag.domain.entity.Task
 import com.example.gdscsohag.ui.base.ContentStatus
 
 data class TasksUiState(
     val contentStatus: ContentStatus = ContentStatus.LOADING,
-    val sessions: List<Session>? = null
+    val sessions: List<Session> = listOf()
 )
--- app/src/main/java/com/example/gdscsohag/ui/tasks/adapter/TasksAdapter.kt
+++ app/src/main/java/com/example/gdscsohag/ui/tasks/adapter/TasksAdapter.kt
@@ -2,10 +2,10 @@ package com.example.gdscsohag.ui.tasks.adapter
 
 import com.example.gdscsohag.R
 import com.example.gdscsohag.databinding.ItemTaskBinding
-import com.example.gdscsohag.domain.entity.Task
+import com.example.gdscsohag.domain.entity.Session
 import com.example.gdscsohag.ui.base.BaseAdapter
 
-class TasksAdapter : BaseAdapter<Task>() {
+class TasksAdapter : BaseAdapter<Session.Task>() {
     override val layoutId = R.layout.item_task
 
     override fun bindData(holder: BaseViewHolder, position: Int) {
